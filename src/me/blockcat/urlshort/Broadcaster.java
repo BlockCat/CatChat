62770753ba71d1171deef60f28c65f9bb3e9f1d0
Zino <Zinoonomiwo@gmail.com>
2012-08-01 19:22:10 +0200

    +Added source

---
+++ src/me/blockcat/urlshort/Broadcaster.java
@@ -0,0 +1,64 @@
+package me.blockcat.urlshort;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Server;
+import org.bukkit.entity.Player;
+
+public class Broadcaster extends Thread {
+	
+	private URL pageURL;
+	private URLshort plugin;
+	private Player player;
+	
+	
+	public Broadcaster(URL url, URLshort instance, Player player) {
+		pageURL = url;
+		plugin = instance;
+		this.player = player;
+	}
+
+	@Override
+	public void run() {
+		String nMsg = "";
+		try {
+		HttpURLConnection urlConnection = (HttpURLConnection) pageURL.openConnection();
+		int respCode = urlConnection.getResponseCode();
+		String response = urlConnection.getResponseMessage();
+
+		InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+		Reader r = new InputStreamReader(in);
+		int c;
+		String all = "";
+
+		while ((c = r.read()) != -1) {
+			String u = String.valueOf((char) c);
+			all += u;
+
+		}
+		String d = Character.toString('"');
+		//event.getPlayer().sendMessage(all.split(d).length + " " + d + u);
+		all = "okij.in/" +all.split(d)[7];
+		nMsg = nMsg + all;
+		in.close();
+		r.close();
+		urlConnection.disconnect();
+		} catch (Exception e) {
+			return;
+		}
+		
+		Server server = plugin.getServer();
+		
+		server.broadcastMessage(ChatColor.GREEN + player.getDisplayName() + ChatColor.YELLOW +" has shared a link:");
+		server.broadcastMessage(ChatColor.GOLD + nMsg);
+		
+		
+	}
+
+}
---
+++ src/me/blockcat/urlshort/URLshort.java
@@ -0,0 +1,55 @@
+package me.blockcat.urlshort;
+
+import java.io.File;
+
+import net.milkbowl.vault.permission.Permission;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class URLshort extends JavaPlugin {
+
+	public static Permission permission = null;
+	public String format;
+	public static FileConfiguration config;
+
+	public void onEnable() {		
+		//setupPermissions();
+		/*config = this.getConfig();
+		File f = new File(this.getDataFolder(), "config.yml");
+		if (f.exists()) {
+			f.mkdir();
+		}
+		
+		if (!config.contains("Format"))
+			config.set("Format", "&f<+prefix&f>+suffix +name&f: +msg");
+		
+		if (!config.contains("Prefix.group"))
+		config.set("Prefix.group", "test");
+		
+		if (!config.contains("Suffix.group")) 
+		config.set("Suffix.group", "test");
+		
+		format = config.getString("Format");
+		this.saveConfig();
+		*/
+		getServer().getPluginManager().registerEvents(new URLListener(this),this);
+	}
+
+	/*private boolean setupPermissions()
+	{
+		RegisteredServiceProvider<Permission> permissionProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.permission.Permission.class);
+		if (permissionProvider != null) {
+			permission = permissionProvider.getProvider();
+		}
+		return (permission != null);
+	}
+	*/
+	
+	public String getFormat() {
+		return format;
+	}
+
+
+}
